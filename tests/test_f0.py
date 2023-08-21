14f546b1a3977f66e5ea9ab6a7f359e24b9a0102
leob <leo.bruetting@fau.de>
2023-08-21 13:55:54 +0200

    modified test_f0

---
+++ tests/test_f0.py
@@ -0,0 +1,38 @@
+import numpy as np
+import os
+from scipy.io import wavfile
+from libsoni.core import f0
+FS = 22050
+C_MAJOR_SCALE = [261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25, 0.0]
+EXPORT_AUDIO = False
+MAKE_GROUND_TRUTH_EXAMPLES = False
+
+
+def test_f0():
+    time_positions = np.arange(0.2, len(C_MAJOR_SCALE) * 0.5, 0.5)
+    time_f0 = np.column_stack((time_positions, C_MAJOR_SCALE))
+    DURATIONS = [None, 4.2, 3.0, 5.0]
+
+    for duration in DURATIONS:
+        if duration is None:
+            duration_in_samples = None
+        else:
+            duration_in_samples = int(duration * FS)
+
+        y = f0.sonify_f0(time_f0=time_f0,
+                         partials=np.array([1]),
+                         partials_amplitudes=np.array([1]),
+                         duration=duration_in_samples,
+                         fs=FS)
+
+        if MAKE_GROUND_TRUTH_EXAMPLES:
+            np.save(os.path.join('tests', 'data', f'C_major_scale_fo_sonified_{duration}'), y)
+
+        if EXPORT_AUDIO:
+            wavfile.write(os.path.join('tests', 'data_audio', f'C_major_scale_fo_sonified_{duration}.wav'), FS, y)
+
+        y_test = np.load(os.path.join('tests','data', f'C_major_scale_fo_sonified_{duration}.npy'))
+
+        assert np.array_equal(y, y_test)
+
+test_f0()
