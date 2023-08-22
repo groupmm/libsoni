d38462c6f706459c9640dd2f39d050792e2a8dcd
leob <leo.bruetting@fau.de>
2023-08-22 12:05:44 +0200

    test-case for sonify_tse_sample()

--- tests/test_tse.py
+++ tests/test_tse.py
@@ -1,3 +1,4 @@
+import librosa
 import numpy as np
 import os
 from scipy.io import wavfile
@@ -6,9 +7,10 @@ from libsoni.core import tse
 FS = 22050
 EXPORT_AUDIO = True
 RELATIVE_OFFSETS = [0.0, 0.5, 1]
-DURATIONS = [None, 4.2, 3.0, 5.0]
+DURATIONS = [None, 0.1, 3.0, 5.0]
 
-def test_tse():
+
+def test_tse_click():
     time_positions = np.arange(0, 5.3, 0.5)
 
     for duration in DURATIONS:
@@ -26,6 +28,32 @@ def test_tse():
                                      fs=FS)
 
             if EXPORT_AUDIO:
-                wavfile.write(os.path.join('tests', 'data_audio', f'start0_time_positions_tse_sonified_{duration}_{relative_offset}.wav'), FS, y)
+                wavfile.write(os.path.join('tests', 'data_audio', f'start0_time_positions_tse_clicks_sonified_{duration}_{relative_offset}.wav'), FS, y)
 
-test_tse()
+#test_tse_click()
+
+def test_tse_sample():
+
+    time_positions = np.arange(0, 5.3, 0.5)
+
+    for wav_name in os.listdir('data_audio/samples'):
+        if '.wav' not in wav_name:
+            continue
+        sample, _ = librosa.load(os.path.join('data_audio/samples', wav_name), sr=FS)
+        filename = wav_name.split('.wav')[0]
+        for duration in DURATIONS:
+            if duration is None:
+                duration_in_samples = None
+            else:
+                duration_in_samples = int(duration * FS)
+            for relative_offset in RELATIVE_OFFSETS:
+                y = tse.sonify_tse_sample(time_positions=time_positions,
+                                          sample=sample,
+                                          offset_relative=relative_offset,
+                                          duration=duration_in_samples)
+
+                if EXPORT_AUDIO:
+                    wavfile.write(os.path.join('tests', 'data_audio',
+                                               f'{filename}_time_positions_tse_samples_sonified_{duration}_{relative_offset}.wav'), FS, y)
+
+test_tse_sample()
