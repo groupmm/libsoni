96416448c88ccb852b17eab041023e127508d7cb
Yigitcan Oezer <yigitcan.oezer@audiolabs-erlangen.de>
2023-08-29 10:44:23 +0200

    setup.py update

--- setup.py
+++ setup.py
@@ -1,3 +1,41 @@
 from setuptools import setup, find_packages
 
-setup(name='libsoni', version='1.0', packages=find_packages())
+
+with open('README.md', 'r') as fdesc:
+    long_description = fdesc.read()
+
+setup(
+    name='libsoni',
+    version='0.0.0',
+    description='Sonification Toolbox',
+    author='',
+    author_email='',
+    url='https://github.com/groupmm/libsoni',
+    download_url='https://github.com/groupmm/libsoni',
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Topic :: Multimedia :: Sound/Audio :: Analysis",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords='audio music sound synchronization dtw mrmsdtw',
+    license='MIT',
+    install_requires=['ipython >= 7.8.0, < 8.0.0',
+                      'librosa >= 0.8.0, < 1.0.0',
+                      'matplotlib >= 3.1.0, < 4.0.0',
+                      'numpy >= 1.17.0, < 2.0.0',
+                      'pandas >= 1.0.0, < 2.0.0',
+                      'pysoundfile >= 0.9.0, < 1.0.0',
+                      'scipy >= 1.7.0, < 2.0.0',
+                      'libfmp >= 1.2.0, < 2.0.0'],
+    python_requires='>=3.7, <4.0',
+    extras_require={
+        'tests': ['pytest == 6.2.*'],
+        'docs': ['sphinx == 6.2.*',
+                 'sphinx_rtd_theme == 1.2.*']
+    }
+)
