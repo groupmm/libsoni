bafd4f7f94896a732a24a4af385911c6e19f1a7a
leob <leo.bruetting@fau.de>
2023-08-31 11:09:05 +0200

    removed audio_player_list()

--- libsoni/util/utils.py
+++ libsoni/util/utils.py
@@ -229,38 +229,3 @@ def envelope_signal(signal: np.ndarray, attack_time: float = 0, decay_time: floa
     enveloped_signal = signal * np.concatenate([attack_func, decay_func, sustain_func, release_func])
 
     return enveloped_signal
-
-# Taken from https://www.audiolabs-erlangen.de/resources/MIR/FMP/B/B_PythonAudio.html
-
-
-def audio_player_list(signals, rates, width=270, height=40, columns=None, column_align='center'):
-    """Generate a list of HTML audio players tags for a given list of audio signals.
-
-    Notebook: B/B_PythonAudio.ipynb
-
-    Args:
-        signals (list): List of audio signals
-        rates (list): List of sample rates
-        width (int): Width of player (either number or list) (Default value = 270)
-        height (int): Height of player (either number or list) (Default value = 40)
-        columns (list): Column headings (Default value = None)
-        column_align (str): Left, center, right (Default value = 'center')
-    """
-    pd.set_option('display.max_colwidth', None)
-
-    if isinstance(width, int):
-        width = [width] * len(signals)
-    if isinstance(height, int):
-        height = [height] * len(signals)
-
-    audio_list = []
-    for cur_x, cur_Fs, cur_width, cur_height in zip(signals, rates, width, height):
-        audio_html = ipd.Audio(data=cur_x, rate=cur_Fs)._repr_html_()
-        audio_html = audio_html.replace('\n', '').strip()
-        audio_html = audio_html.replace('<audio ', f'<audio style="width: {cur_width}px; height: {cur_height}px" ')
-        audio_list.append([audio_html])
-
-    df = pd.DataFrame(audio_list, index=columns).T
-    table_html = df.to_html(escape=False, index=False, header=bool(columns))
-    table_html = table_html.replace('<th>', f'<th style="text-align: {column_align}">')
-    ipd.display(ipd.HTML(table_html))
