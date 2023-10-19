298df7dd96729962c595a7656b2d899334661aa3
Leo Brütting <leo.leonardo97@gmx.de>
2023-10-19 09:13:38 +0200

    new function sonify_chroma_vector

--- libsoni/core/chroma.py
+++ libsoni/core/chroma.py
@@ -5,10 +5,77 @@ from libsoni.util.utils import normalize_signal, fade_signal, smooth_weights
 from libsoni.core.methods import generate_shepard_tone
 
 
-def sonify_chroma_vector():
-    #TODO: DocString
-    #TODO: Implement
-    return
+def sonify_chroma_vector(chroma_vector: np.ndarray,
+                         pitch_range: Tuple[int, int] = (20, 108),
+                         filter: bool = False,
+                         f_center: float = 440.0,
+                         octave_cutoff: int = 1,
+                         sonification_duration: int = None,
+                         fade_duration: float = 0.05,
+                         normalize: bool = True,
+                         fs: int = 22050,
+                         tuning_frequency: float = 440.0) -> np.ndarray:
+
+    """Sonify chroma vector
+
+        Parameters
+        ----------
+        chroma_vector: np.ndarray
+            Chroma data to sonify.
+        pitch_range: Tuple[int, int], default = [20,108]
+            pitches to encounter in shepard tone
+        filter: bool, default: False
+            decides, if shepard tones are filtered or not
+        f_center : float, default: 440.0
+            center_frequency in Hertz for bell-shaped filter
+        octave_cutoff: int, default: 1
+            determines, at which multiple of f_center, the harmonics get attenuated by 2.
+        sonification_duration: int, default = None
+            Duration of audio, given in samples
+        fade_duration: float, default = 0.05
+            Duration of fade-in and fade-out at beginning and end of the sonification, given in seconds.
+        fs: int, default: 44100
+            sampling rate in Samples/second
+        normalize: bool, default = True
+            Decides, if output signal is normalized to [-1,1].
+        fs: int, default = 22050
+            Sampling rate, in samples per seconds.
+        tuning_frequency : float, default = 440
+            Tuning frequency.
+
+        Returns
+        -------
+            y: synthesized tone
+        """
+    assert chroma_vector.shape[0] == 12, f'The chromagram must have shape 12xN.'
+
+    # Determine length of sonification
+    num_samples = sonification_duration
+
+    # Initialize sonification
+    chroma_sonification = np.zeros(num_samples)
+
+    for pitch_class in range(12):
+        if chroma_vector[pitch_class] > 0:
+
+            shepard_tone = generate_shepard_tone(pitch_class=pitch_class,
+                                                 pitch_range=pitch_range,
+                                                 filter=filter,
+                                                 f_center=f_center,
+                                                 octave_cutoff=octave_cutoff,
+                                                 gain=chroma_vector[pitch_class],
+                                                 duration_sec=num_samples / fs,
+                                                 fs=fs,
+                                                 f_tuning=tuning_frequency,
+                                                 fading_sec=0)
+
+            chroma_sonification += shepard_tone
+
+    chroma_sonification = fade_signal(chroma_sonification, fs=fs, fading_sec=fade_duration)
+
+    chroma_sonification = normalize_signal(chroma_sonification) if normalize else chroma_sonification
+
+    return chroma_sonification
 
 
 def sonify_chromagram(chromagram: np.ndarray,
