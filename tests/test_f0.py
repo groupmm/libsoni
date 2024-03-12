267d9cacdf74519dee40fadf39121fbaf5eff34f
yiitozer <yigitcan.oezer@audiolabs-erlangen.de>
2024-03-12 11:06:59 +0100

    tests added for f0

--- tests/test_f0.py
+++ tests/test_f0.py
@@ -1,36 +1,32 @@
 import numpy as np
+import soundfile as sf
+
 from libsoni.core import f0
 
-FS = 22050
+Fs = 22050
 C_MAJOR_SCALE = [261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25, 0.0]
-EXPORT_AUDIO = False
-MAKE_GROUND_TRUTH_EXAMPLES = False
-DURATIONS = [None, 4.2, 3.0, 5.0]
+DURATIONS = [None, 3.0, 5.0]
+PARTIALS = [np.array([1]), np.array([1, 2, 3])]
+PARTIALS_AMPLITUDES = [np.array([1]), np.array([1, 0.5, 0.25])]
+
 
 def test_f0():
     time_positions = np.arange(0.2, len(C_MAJOR_SCALE) * 0.5, 0.5)
     time_f0 = np.column_stack((time_positions, C_MAJOR_SCALE))
 
     for duration in DURATIONS:
-        if duration is None:
-            duration_in_samples = None
-        else:
-            duration_in_samples = int(duration * FS)
+        for par_idx, partials in enumerate(PARTIALS):
+            if duration is None:
+                duration_in_samples = None
+            else:
+                duration_in_samples = int(duration * Fs)
 
-        y = f0.sonify_f0_dev(time_f0=time_f0,
-                         partials=np.array([1]),
-                         partials_amplitudes=np.array([1]),
-                         sonification_duration=duration_in_samples,
-                         fs=FS)
-        '''
-        if MAKE_GROUND_TRUTH_EXAMPLES:
-            np.save(os.path.join('tests', 'data', f'C_major_scale_fo_sonified_{duration}'), y)
+            y = f0.sonify_f0(time_f0=time_f0,
+                             partials=partials,
+                             partials_amplitudes=PARTIALS_AMPLITUDES[par_idx],
+                             sonification_duration=duration_in_samples,
+                             fs=Fs)
 
-        if EXPORT_AUDIO:
-            wavfile.write(os.path.join('tests', 'data_audio', f'C_major_scale_fo_sonified_{duration}.wav'), FS, y)
-
-        y_test = np.load(os.path.join('tests', 'data', f'C_major_scale_fo_sonified_{duration}.npy'))
-        '''
-        assert np.array_equal(y, y)
-
-test_f0()
+            ref, _ = sf.read(f'tests/data/f0_{duration_in_samples}_{par_idx}.wav')
+            assert len(y) == len(ref), 'Length of the generated sonification does not match with the reference!'
+            assert np.allclose(y, ref, atol=1e-4, rtol=1e-5)
